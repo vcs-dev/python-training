1bf2f7cc680fb5b1188a7f78f58488e521198f6b
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2023-03-03 19:01:00 -0300

    number handling

---
+++ core/number-handling.py
@@ -0,0 +1,26 @@
+n1 = 10
+n2 = 24.5
+
+#sum of integer and decimal
+print(n1 + n2)
+
+#subtraction of integer and decimal
+print(n1 - n2)
+
+#multiplication
+print(n1 * n2)
+
+#n1 to the power of 3
+print(n1 ** 3)
+
+#n1 divided by 3
+print(n1 / 3)
+
+#n1 divided by 3 ignoring the decimal part
+print(n1 // 3)
+
+#modulus of n1 divided by 3
+print(n1 % 3)
+
+#precedence of operators
+print(30 * 2 ** 3 - 8 / 4) #The same as (30 * (2 ** 3)) - (8 / 4)
