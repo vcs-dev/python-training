967ec26c8ad518121acc4898fff256cd6ac30d53
Vinicius Campos da Silva <vini.campos.silva@gmail.com>
2023-03-06 22:19:41 -0300

    Input training

---
+++ core/receive-data-and-print-it.py
@@ -0,0 +1,8 @@
+#receive string and print it
+x = input("Type something: ")
+print(x)
+
+#receive int and sum it and then print the result
+y = int(input("Inform a number: "))
+z = 30
+print("Your number plus " + str(z) + " is " + str(y + z))
